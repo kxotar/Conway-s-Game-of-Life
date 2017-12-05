a02a367cfe42f774f9a02b0e6a36e93838c6acbc
Rakesh Sharma <sharma26.riti@gmail.com>
2017-12-05 13:14:06 +0530

    comment removed

--- test_location.rb
+++ test_location.rb
@@ -29,48 +29,7 @@ describe Location do
   it 'West is located at (-1, 0)' do
     assert_equal [-1, 0], Location::WEST
   end
-=begin
-  it 'location for 0,1 is north' do
-    location =Location.for(0, 1)
-    assert_equal Location::NORTH, location
-  end
-  it 'location for -1, 1 is north west' do
-    location = Location.for(-1, 1)
-    assert_equal Location::NORTHWEST, location
-  end
-  it 'location for 1, 1 is north east' do
-    location = Location.for(1, 1)
-    assert_equal Location::NORTHEAST, location
-  end
-  it 'location for -1,-1 is south west' do
-    location= Location.for(-1, -1)
-    assert_equal Location::SOUTHWEST, location
-  end
-  it 'location for 1, 1 is south east' do
-    location = Location.for(1, -1)
-    assert_equal Location::SOUTHEAST, location
-  end
-  it 'location for 0, 1 is north' do
-    location = Location.for(0, 1)
-    assert_equal Location::NORTH, location
-  end
-  it 'location for 0, -1 is south' do
-    location = Location.for(0, -1)
-    assert_equal Location::SOUTH, location
-  end
-  it 'location for 1, 0 is south' do
-    location = Location.for(1, 0)
-    assert_equal Location::EAST, location
-  end
-  it 'location for -1, 0 is south' do
-    location = Location.for(-1, 0)
-    assert_equal Location::WEST, location
-  end
-  it 'location for 0, 0 is center' do
-    location = Location.for(0, 0)
-    assert_equal Location::CENTER, location
-  end
-=end
+
   it 'adding two locations returns a new location' do
     result = Location.add(Location::SOUTH, Location::CENTER)
     assert_equal Location::SOUTH, result
