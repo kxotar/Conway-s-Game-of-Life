70d1b146c6a674bf1a4468343a6db183ff491a81
Rakesh Sharma <sharma26.riti@gmail.com>
2017-12-05 13:18:00 +0530

    comments removed

--- location.rb
+++ location.rb
@@ -10,30 +10,6 @@ class Location
   WEST= [-1, 0]
 
     OFFSETS = [NORTHWEST, NORTHEAST, SOUTHWEST, SOUTHEAST, NORTH, SOUTH, EAST, WEST]
-=begin
-  def self.for(x, y)
-    if x == 0 and y == 1
-      NORTH
-    elsif x == 0 and y == -1
-      SOUTH
-    elsif x == 1 and y == 0
-      EAST
-    elsif x == -1 and y == 0
-      WEST
-    elsif x == 0 and y == 0
-      CENTER
-    elsif x == -1 and y == 1
-      NORTHWEST
-    elsif x == 1 and y == 1
-      NORTHEAST
-    elsif x == 1 and y == -1
-      SOUTHEAST
-    elsif x == -1 and y == -1
-      SOUTHWEST
-    end
-  end
-=end
-
   def self.add(first, second)
     [first[0]+second[0], first[1]+second[1]]
   end
